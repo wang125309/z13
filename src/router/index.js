d04288be7394159d9bfcb0c6993498a5869fb8d6
wang125309 <wang_125309@foxmail.com>
2018-06-30 23:16:27 +0800

    m

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,16 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 
-Vue.use(Router)
+const Home = r => require.ensure([], ()=>r(require('@/components/Home.vue')))
+Vue.use(Router);
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+    scrollBehavior: () => ({y: 0}),
+    routes: [
+        {
+            path: '/',
+            name: 'Home',
+            component: Home
+        }
+    ]
 })
