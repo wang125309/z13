c44b50e13e5d96dee1d852cc73d27fb7c32322b0
wang_125309 <wang_125309@foxmail.com>
2018-07-13 17:59:56 +0800

    session

--- src/Session/cookie.js
+++ src/Session/cookie.js
@@ -3,7 +3,7 @@ import Cookies from 'js-cookie'
 const cookies = {}
 
 cookies.setCookie = (token) => {
-    Cookies.set('token', token);
+    Cookies.set('token', token, { expires: 7 });
 }
 
 cookies.getCookie = () => {
