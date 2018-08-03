72d059ccb1e97bad835ef3cce7b90c9adddcf4b6
wang_125309 <wang_125309@foxmail.com>
2018-08-03 11:34:10 +0800

    sdf

--- src/components/Water.vue
+++ src/components/Water.vue
@@ -42,7 +42,11 @@
         <ActivityCardItem cut full title="服务说明">
             <div class="description">{{data.description}}</div>
         </ActivityCardItem>
-        <ActivityCardItem no-padding  cut full title="商品规格">
+        <ActivityCardItem
+            no-padding
+            cut
+            full
+            title="商品规格">
             <Good v-for="i in data.waterInfos"
                   :key="'good' + i.id"
                   :title="i.name"
