e4b0ea40c6768fa9019ebfcc8a539e2eac96f0ec
jarvis <leguroky@gmail.com>
2018-11-02 11:35:07 +0800

    refine

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const server = express()
 server.use('/static/', express.static(__dirname + '/dist/static'))
 server.get('/', (req, res) => res.sendFile(__dirname + '/dist/index.html'))
 
+server.get('/MP_verify_fHJhpA2HZCwUCCXa.txt', (req, res) => {
+	res.sendFile(__dirname + '/MP_verify_fHJhpA2HZCwUCCXa.txt')
+})
+
 const PORT = 8080
 server.listen(PORT, () => {
     console.log('listening on port', PORT)
