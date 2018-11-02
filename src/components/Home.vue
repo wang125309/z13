92721cca04b227038a8923dc722ba12d94430a87
wang_125309 <wang_125309@foxmail.com>
2018-11-02 16:20:48 +0800

    fix

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -183,7 +183,6 @@
                     observer: true,//修改swiper自己或子元素时，自动初始化swiper
                     observeParents: true,//修改swiper的父元素时，自动初始化swiper
                     slidesPerView: 'auto',
-                    reverseDirection: true
                 },
                 data: {
                     newsAndRunableUnit: {
